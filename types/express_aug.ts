2448acefad0ffd818486a659f0d1dba56487f29b
Acoolwolf96 <njoku.nelson@student.hamk.fi>
2025-06-08 22:17:43 +0300

    modification on server

--- types/express_aug.ts
+++ types/express_aug.ts
@@ -3,7 +3,7 @@ import { JwtPayload } from '../utils/token';
 declare global {
     namespace Express {
         interface Request {
-            user?: JwtPayload | string;
+            user: JwtPayload;
         }
     }
 }
