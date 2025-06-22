cab34f4004a68ac4febe856a8450b13925faf48d
Acoolwolf96 <njoku.nelson@student.hamk.fi>
2025-06-22 22:11:43 +0300

    cookies patches

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -2,7 +2,7 @@ import { CookieOptions } from 'express';
 
 export const cookieConfig: CookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
-    path: '/v4',
+    secure: true,
+    sameSite: 'none',
+    path: '/',
 };
